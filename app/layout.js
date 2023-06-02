fd427c35074b8d11c594ba62d82a0956de3127fd
wahiditckd <wahid.itckd@gmail.com>
2023-06-02 21:38:39 +0700

    proyek landingpage2

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { poppins } from '@/lib/utils'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
